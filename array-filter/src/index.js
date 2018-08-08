276dc7c7017992e8882d3fdbef8f7f50884c1ac8
NataliyaShipelova <n.shipelova@tradesoft.ru>
2018-08-08 11:50:10 +0300

    Произведена установка Redux, создан store, написан первоначальный reducer

--- array-filter/src/index.js
+++ array-filter/src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from "redux";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const initialState = {};
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "GENERATE_ARRAY":
+      return {
+        ...state,
+        array: action.array
+      };
+    default:
+      return state;
+  }
+}
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>,
+document.getElementById('root'));
 registerServiceWorker();
